4aac382471a5af4199b5996e024ff5e19c4d0c48
JohnPantsWork <JohnPantsWork@gmail.com>
2021-08-16 01:12:18 +0800

    [Aming] basic unity and coordinate basic build update

---
+++ UdemyUnityCourse05/Assets/CoordinateLabel.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[ExecuteAlways]
+public class CoordinateLabel : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+
+        coordinates.x = Mathf.RoundToInt(transform.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.position.z / UnityEditor.EditorSnapSettings.move.z);
+        Debug.Log(coordinates.x + "," + coordinates.y);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
